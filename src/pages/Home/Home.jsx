c60a5e3b286ffacd022db29aaae62d5facd7fc58
Aman Singh <116139327+amancd@users.noreply.github.com>
2024-08-19 19:40:01 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import hero_banner from '../../../public/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
-import play_icon from '../../assets/Play_icon.png'
+import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards'
 
